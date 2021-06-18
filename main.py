95fbc039602a7bae574e1bf236fac59fa0febafb
Li, Linfeng <li.linfeng.xjtu@gmail.com>
2021-06-18 09:08:33 +0100

    new-only-displacement

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from evtu import *
 
 ss = 1e-5
 
-tmax = 85
+tstart = 00
+tmax = 190
+tstep = 10
 # axis
 p1 = (0, 0, 0)
 p2 = (0, 0, 0.628)
@@ -16,40 +18,50 @@ p4 = (0, -0.05+ss, 0.314)
 
 # generate points on line. store their coordinate to nparray "line"
 line = lineCoor(p1,p2,resol)
+print(line)
 
 # define objects to store results
 f1 = open('out1.txt', 'w') # to store material points on 'line' and their spatial coordinates
-f2 = open('out2.txt', 'w') # to store stress of points p3, p4 and their 
-x_mat = np.zeros([line.shape[0], 3])
+# f2 = open('out2.txt', 'w') # to store stress of points p3, p4 and their 
+# x_mat = np.zeros([line.shape[0], 3])
 x_spatial = np.zeros([line.shape[0], 3])
 
 # loop over time steps
 
 # a prograss indication bar
-printProgressBar(0, tmax+1, prefix = 'Progress', suffix = 'Complete', length = 50)
+printProgressBar(tstart, tmax+1, prefix = 'Progress', suffix = 'Complete', length = 50)
 
-for ts in range(tmax+1):
+file_directory = '/data/linfeng/data2/module_vldt/z18_gravity/liang/z02-nonlinear5/restart2/'
+# '/data/linfeng/data2/module_vldt/z18_gravity/fix-two-ends/finer-uniform-adapt/restart-from-500/'
+
+for ts in range(tstart,tmax+tstep, tstep):
     # The data file
-    file_name = "/data2/linfeng/module_vldt/z08_xAdapt/solid_vldt_set3_"\
-	+str(ts)+".pvtu"
+    file_name = file_directory + "solid_vldt_set6+6100_"+str(ts)+".pvtu"
     #"solid_vldt_set6xVelAdapt_"+str(ts)+".pvtu"
     t88 = evtu(file_name)
     # new coordinate
-    (x_mat, x_spatial)=t88.newCoordinateatLine(line)
-    for i in range(x_mat.shape[0]):
-        f1.write('%16.10e\t%16.10e\t%16.10e\t'%(x_mat[i,0], x_mat[i,1], x_mat[i,2]))
-    for i in range(x_mat.shape[0]):
-        f1.write('%16.10e\t%16.10e\t%16.10e\t'%(x_spatial[i,0], x_spatial[i,1], x_spatial[i,2]))
-    f1.write('\n')
+    x_spatial=t88.newCoordinateatLine(line)
+    if (ts==0):
+        for i in range(line.shape[0]):
+            f1.write('%16.10e\t%16.10e\t%16.10e\t'%(line[i,0], line[i,1], line[i,2]))
+        f1.write('\n')
+        continue
+    else:
+        for i in range(x_spatial.shape[0]):
+            f1.write('%16.10e\t%16.10e\t%16.10e\t'%(x_spatial[i,0], x_spatial[i,1], x_spatial[i,2]))
+        f1.write('\n')
 
-    # stress at points
-    for p in (p3,p4):
-        stress = t88.getStress(p)
-        [f2.write('%16.10e\t'%stress[i,j]) for i in range(3) for j in range(3)]
-    f2.write('\n')
+    # # stress at points
+    # for p in (p3,p4):
+    #     stress = t88.getStress(p)
+    #     [f2.write('%16.10e\t'%stress[i,j]) for i in range(3) for j in range(3)]
+    # f2.write('\n')
 
     # update progress bar
     printProgressBar(ts, tmax+1, prefix = 'Progress', suffix = 'Complete', length = 50)
 
+f1.write(file_directory)
+f1.write('\ntmax = %d\n'%tmax)
+f1.write('tstep = %d'%tstep)
 f1.close()
-f2.close()
+# f2.close()
